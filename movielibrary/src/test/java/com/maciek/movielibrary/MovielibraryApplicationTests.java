34719d1b9d3def676e9eda74f2f70d48dbc4569a
Maciej Adamczuk <73827356+rudytako@users.noreply.github.com>
2023-05-15 21:45:40 +0200

    Update MovielibraryApplicationTests.java

--- movielibrary/src/test/java/com/maciek/movielibrary/MovielibraryApplicationTests.java
+++ movielibrary/src/test/java/com/maciek/movielibrary/MovielibraryApplicationTests.java
@@ -1,13 +1,35 @@
 package com.maciek.movielibrary;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
-class MovielibraryApplicationTests {
+class MovieLibraryApplicationTests {
+	@Autowired
+	private MovieRepository movieRepository;
 
 	@Test
-	void contextLoads() {
+	void getById_NotExistingMovie_ReturnNull() {
+		MovieController movieController = new MovieController();
+		movieController.movieRepository = movieRepository;
+
+		// Try to get a movie with id = 5 that does not exist
+		assertNull(movieController.getById(5));
+	}
+
+	@Test
+	void update_NotExistingMovie_ReturnZero() {
+		MovieController movieController = new MovieController();
+		movieController.movieRepository = movieRepository;
+
+		Movie updatedMovie = new Movie();
+		updatedMovie.setName("Updated Movie");
+		updatedMovie.setRating(4);
+
+		assertEquals(0, movieController.update(5, updatedMovie));
 	}
 
 }
